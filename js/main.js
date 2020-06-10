4b75831db0c3e0a3c4a90135d6952eaa45692866
ildarjan <nobap84@rambler.ru>
2020-06-10 13:06:36 +0400

    Refactor after 1st review. All magic nums rewrite as object properties

--- js/main.js
+++ js/main.js
@@ -1,108 +1,145 @@
 'use strict';
+
+var hotelDefaultProperties = {
+  location: {
+    X_MIN: 330,
+    X_MAX: 830,
+    Y_MIN: 130,
+    Y_MAX: 630
+  },
+  price: {
+    MIN: 100,
+    MAX: 800
+  },
+  rooms: {
+    MIN: 1,
+    MAX: 10
+  },
+  guests: {
+    MIN: 1,
+    MAX: 10
+  }
+};
+var pinRenderProperties = {
+  OFFSETX: -25,
+  OFFSETY: -70
+};
+
+var hotelTitles = ['Hotel Yamanote Otsuka Eki Tower',
+  'HOTEL FELICE Akasaka by RELIEF',
+  'GrandPalace Allamanda Aoyama',
+  'Rixos Premium Kazan JBR',
+  'Shah Palace yamne uren',
+  'Brandisimo Millennium Super Flat',
+  'Jumeirah Al Naseem',
+  'Mövenpick Jumeirah Lakes'];
+var hotelPhotos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+
+var featuresOptions = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var hotelTypes = ['palace', 'flat', 'house', 'bungalo'];
+var checkInOptions = ['12:00', '13:00', '14:00'];
+var checkOutOptions = ['12:00', '13:00', '14:00'];
+
 var mapSection = document.querySelector('section.map');
 mapSection.classList.remove('map--faded');
 
-var templateCard = document.querySelector('template#card').content.querySelector('article');
-var templatePin = document.querySelector('template#pin').content.querySelector('.map__pin');
+var cardTemplate = document.querySelector('template#card').content.querySelector('.map__card.popup');
+var pinTemplate = document.querySelector('template#pin').content.querySelector('.map__pin');
 
-renderTemplates(templateCard, templatePin);
+renderHotels(cardTemplate, pinTemplate);
 
-function renderTemplates(cardTemplate, pinTemplate) {
+function renderHotels(card, pin) {
   var mapPins = mapSection.querySelector('.map__pins');
-  var hotels = getFakeObjectsArray(8);
+  var hotels = generateHotelsArray(8, hotelTitles);
   for (var i = 0; i < hotels.length; i++) {
-    var cardClone = cardTemplate.cloneNode(true);
-    var pinClone = pinTemplate.cloneNode(true);
-    renderCardTemplate(cardClone, hotels[i]);
-    renderPinTemplate(pinClone, hotels[i]);
-    mapPins.appendChild(cardClone);
-    mapPins.appendChild(pinClone);
+    renderCardTemplate(mapPins, card, hotels[i]);
+    renderPinTemplate(mapPins, pin, hotels[i]);
   }
 }
-
-function getFakeObjectsArray(quantity) {
+function generateHotelsArray(quantity, titles) {
   var data = [];
-  var hotelTitles = ['Hotel Yamanote Otsuka Eki Tower',
-    'HOTEL FELICE Akasaka by RELIEF',
-    'GrandPalace Allamanda Aoyama',
-    'Rixos Premium Kazan JBR',
-    'Shah Palace yamne uren',
-    'Brandisimo Millennium Super Flat',
-    'Jumeirah Al Naseem',
-    'Mövenpick Jumeirah Lakes'];
-  var hotelPhotos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg',
-    'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
-    'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 
   for (var i = 1; i <= quantity; i++) {
     var obj = {};
     obj.author = {avatar: 'img/avatars/user0' + i + '.png'};
     obj.location = {
-      x: randomInt(330, 830),
-      y: randomInt(130, 630)
+      x: getRandomInteger(hotelDefaultProperties.location.X_MIN, hotelDefaultProperties.location.X_MAX),
+      y: getRandomInteger(hotelDefaultProperties.location.Y_MIN, hotelDefaultProperties.location.Y_MAX)
     };
     obj.offer = {
-      title: hotelTitles[i - 1],
+      title: titles[i - 1],
       address: obj.location.x + ', ' + obj.location.y,
-      // address: '' + randomInt(100, 800) + ', ' + randomInt(100, 800) + '',
-      price: randomInt(100, 800),
-      type: ['palace', 'flat', 'house', 'bungalo'][randomInt(0, 3)],
-      rooms: randomInt(1, 10),
-      guests: randomInt(1, 10),
-      checkin: ['12:00', '13:00', '14:00'][randomInt(0, 2)],
-      checkout: ['12:00', '13:00', '14:00'][randomInt(0, 2)],
-      features: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'].slice(0, [randomInt(1, 5)]),
-      description: 'Best place. Ever!',
-      photos: hotelPhotos.slice(0, randomInt(1, 2))
+      price: getRandomInteger(hotelDefaultProperties.price.MIN, hotelDefaultProperties.price.MAX),
+      type: getRandomHotelType(),
+      rooms: getRandomInteger(hotelDefaultProperties.rooms.MIN, hotelDefaultProperties.rooms.MAX),
+      guests: getRandomInteger(hotelDefaultProperties.guests.MIN, hotelDefaultProperties.guests.MAX),
+      checkin: getRandomCheckInTime(),
+      checkout: getRandomCheckOutTime(),
+      features: getRandomFeatures(),
+      description: 'Великолепная квартира-студия в центре Токио. Подходит как туристам, так и бизнесменам. Квартира полностью укомплектована и недавно отремонтирована.',
+      photos: getRandomPhotos()
     };
-
     data.push(obj);
   }
   return data;
 }
+function renderCardTemplate(parent, card, hotel) {
+  var cardClone = card.cloneNode(true);
 
-function renderCardTemplate(card, hotel) {
-  var cardChildren = card.querySelectorAll('[class^=popup]');
-  var featuresRex = new RegExp(hotel.offer.features.join('|'), 'gi');
-  for (var j = 0; j < cardChildren.length; j++) {
-    if (cardChildren[j].classList.contains('popup__avatar')) {
-      cardChildren[j].src = hotel.author.avatar;
-    } else if (cardChildren[j].classList.contains('popup__title')) {
-      cardChildren[j].textContent = hotel.offer.title;
-    } else if (cardChildren[j].classList.contains('popup__text--address')) {
-      cardChildren[j].textContent = hotel.offer.address;
-    } else if (cardChildren[j].classList.contains('popup__text--price')) {
-      cardChildren[j].innerHTML = '' + hotel.offer.price + '&#x20bd;<span>/ночь</span>';
-    } else if (cardChildren[j].classList.contains('popup__type')) {
-      cardChildren[j].textContent = hotel.offer.type;
-    } else if (cardChildren[j].classList.contains('popup__capacity')) {
-      cardChildren[j].textContent = '' + hotel.offer.rooms + ' комнаты для ' + hotel.offer.guests + ' гостей';
-    } else if (cardChildren[j].classList.contains('popup__time')) {
-      cardChildren[j].textContent = 'Заезд после' + hotel.offer.checkin + ', выезд до ' + hotel.offer.checkout + '';
-    } else if (cardChildren[j].classList.value.match(/(feature--)/gi) && !cardChildren[j].classList.value.match(featuresRex)) {
-      cardChildren[j].remove();
-    } else if (cardChildren[j].classList.contains('popup__description')) {
-      cardChildren[j].textContent = hotel.offer.description;
-    } else if (cardChildren[j].classList.contains('popup__photos')) {
-      cardChildren[j].children[0].src = hotel.offer.photos[0];
-      if (hotel.offer.photos.length > 1) {
-        for (var k = 1; k < hotel.offer.photos.length; k++) {
-          var image = cardChildren[j].children[0].cloneNode();
-          image.src = hotel.offer.photos[k];
-          cardChildren[j].appendChild(image);
-        }
-      }
-    }
+  cardClone.querySelector('.popup__avatar').src = hotel.author.avatar;
+  cardClone.querySelector('.popup__title').textContent = hotel.offer.title;
+  cardClone.querySelector('.popup__text--address').textContent = hotel.offer.address;
+  cardClone.querySelector('.popup__text--price').innerHTML = hotel.offer.price + '&#x20bd;<span>/ночь</span>';
+  cardClone.querySelector('.popup__type').textContent = hotel.offer.type;
+  cardClone.querySelector('.popup__text--capacity').textContent = hotel.offer.rooms + ' комнаты для ' + hotel.offer.guests + ' гостей';
+  cardClone.querySelector('.popup__text--time').textContent = 'Заезд после' + hotel.offer.checkin + ', выезд до ' + hotel.offer.checkout;
+  cardClone.querySelector('.popup__description').textContent = hotel.offer.description;
+
+  renderCardFeatures(cardClone, hotel);
+  renderCardPhotos(cardClone, hotel);
+
+  parent.append(cardClone);
+}
+function renderPinTemplate(parent, pin, hotel) {
+  var pinClone = pin.cloneNode(true);
+  pinClone.children[0].src = hotel.author.avatar;
+  pinClone.children[0].alt = hotel.offer.title;
+  pinClone.style = 'left:' + (hotel.location.x + pinRenderProperties.OFFSETX) + 'px; top: ' + (hotel.location.y + pinRenderProperties.OFFSETY) + 'px;';
+  parent.append(pinClone);
+}
+function renderCardFeatures(card, hotel) {
+  var features = card.querySelectorAll('.popup__feature');
+  var regex = new RegExp(hotel.offer.features.join('|'), 'gi');
+  features.forEach(function (feature) {
+    return !feature.classList.value.match(regex) ? feature.remove() : '';
+  });
+}
+function renderCardPhotos(card, hotel) {
+  var photosContainer = card.querySelector('.popup__photos');
+  var photo = card.querySelector('.popup__photo');
+  for (var i = 0; i < hotel.offer.photos.length; i++) {
+    var nextPhoto = i === 0 ? photo : photo.cloneNode();
+    nextPhoto.src = hotel.offer.photos[i];
+    photosContainer.append(nextPhoto);
   }
 }
-
-function renderPinTemplate(pin, hotel) {
-  pin.children[0].src = hotel.author.avatar;
-  pin.children[0].alt = hotel.offer.title;
-  pin.style = 'left:' + (hotel.location.x - 25) + 'px; top: ' + (hotel.location.y - 70) + 'px;';
+function getRandomPhotos() {
+  return hotelPhotos.slice(0, getRandomInteger(1, hotelPhotos.length - 1));
 }
-
-function randomInt(min, max) {
-  var ans = min + Math.floor(Math.random() * (max + 1 - min));
-  return ans;
+function getRandomFeatures() {
+  return featuresOptions.slice(0, getRandomInteger(1, featuresOptions.length - 1));
+}
+function getRandomHotelType() {
+  return hotelTypes[getRandomInteger(0, hotelTypes.length - 1)];
+}
+function getRandomCheckInTime() {
+  return checkInOptions[getRandomInteger(0, checkInOptions.length - 1)];
+}
+function getRandomCheckOutTime() {
+  return checkOutOptions[getRandomInteger(0, checkOutOptions.length - 1)];
+}
+function getRandomInteger(min, max) {
+  return min + Math.floor(Math.random() * (max + 1 - min));
 }
