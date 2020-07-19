b7c2dd78c42956b5fe96d0884c9b58964b6d0324
ildarjan <nobap84@rambler.ru>
2020-07-20 03:55:50 +0400

    cut out pins handlers and replace to separate moduleform-filter behavior

--- js/main.js
+++ js/main.js
@@ -9,26 +9,24 @@
   var activateFilters = window.keksobookingMap.activateFilters;
   var activateForm = window.guestNoticeForm.activateForm;
   var activatePins = window.hotelsPins.activatePins;
+  var applyFormFilters = window.filterForm.applyPinsFilters;
   var deactivateFilters = window.keksobookingMap.deactivateFilters;
   var deactivateForm = window.guestNoticeForm.deactivateForm;
   var deactivateMap = window.keksobookingMap.deactivateMap;
-  var debounceFilters = window.utilities.debounce(applyPinsFilters, DEBOUNCE_DELAY);
   var convertHotelsResponse = window.hotelsCards.convertHotelsResponse;
-  var getPinId = window.hotelsPins.getPinId;
-  var mapSection = window.keksobookingMap.mapSection;
   var mainPin = window.keksobookingMap.mainPin;
   var renderHotelsPins = window.hotelsPins.renderPins;
   var renderHotelCard = window.hotelsCards.renderCard;
-  var removeCard = window.hotelsCards.removeCard;
-  var removeCurrentCard = window.hotelsCards.removeCurrentCard;
   var removeRenderedPins = window.hotelsPins.removeRenderedPins;
+
+  var debouncedFilters;
   var hotels;
 
   function disableKeksobooking() {
     mainPin.addEventListener('mousedown', mainPinMousedownHandler);
     mainPin.addEventListener('keydown', mainPinKeydownHandler);
     deactivateMap();
-    removeRenderedPins(pinClickHandler, pinKeyEnterHandler);
+    removeRenderedPins();
     deactivateFilters();
     deactivateForm();
   }
@@ -58,157 +56,35 @@
     hotels = convertHotelsResponse(response);
     var ids = Object.keys(hotels).slice(0, MAX_PINS_DISPLAY);
     renderHotelsPins(ids, hotels);
-    activatePins(pinClickHandler, pinKeyEnterHandler);
+    activatePins();
+    applyFormFilters = applyFormFilters.bind(null, hotels, MAX_PINS_DISPLAY);
+    debouncedFilters = window.utilities.debounce(applyFormFilters, DEBOUNCE_DELAY);
   }
 
   function errorCallback(message) {
     window.popupMessage.show(message);
   }
 
-  function pinClickHandler(event) {
-    var pinId = getPinId(event.target);
-    var currentCard = mapSection.querySelector('.map__card.popup');
-    if (currentCard && currentCard.dataset.id === pinId) {
-      return;
-    }
-    if (currentCard && currentCard.dataset.id !== pinId) {
-      removeCard(currentCard);
-    }
-    renderHotelCard(hotels[pinId]);
+  function renderPinById(id) {
+    renderHotelCard(hotels[id]);
   }
 
-  function pinKeyEnterHandler(event) {
-    if (event.key === 'Enter') {
-      event.preventDefault();
-      var pinId = getPinId(event.target);
-      var currentCard = mapSection.querySelector('.map__card.popup');
-      if (currentCard && currentCard.dataset.id === pinId) {
-        return;
-      }
-      if (currentCard && currentCard.dataset.id !== pinId) {
-        removeCard(currentCard);
-      }
-      renderHotelCard(hotels[pinId]);
-    }
-  }
   // ####################################
   // #####      MODULE7-TASK2       #####
   // #####            &&            #####
   // #####      MODULE7-TASK3       #####
   // ####################################
 
-  var hotelFilters = {
-    'housing-type': filterByType,
-    'housing-price': filterByPrice,
-    'housing-guests': filterByGuests,
-    'housing-rooms': filterByRooms,
-    'features': filterByFeatures
-  };
-
-  var priceRangeDescription = {
-    'low': {
-      min: 0,
-      max: 10000,
-    },
-    'middle': {
-      min: 10000,
-      max: 50000,
-    },
-    'high': {
-      min: 50000,
-      max: 1000000000,
-    },
-  };
-
   var mapFilters = window.keksobookingMap.mapFilters;
-
   mapFilters.addEventListener('change', function () {
-    debounceFilters();
+    debouncedFilters();
   });
-
-
-  function applyPinsFilters() {
-    var filteredIds = getFilteredHotelsIds(mapFilters).slice(0, MAX_PINS_DISPLAY);
-    removeRenderedPins(pinClickHandler, pinKeyEnterHandler);
-    removeCurrentCard();
-    renderHotelsPins(filteredIds, hotels);
-    activatePins(pinClickHandler, pinKeyEnterHandler);
-  }
-
-  function getFilteredHotelsIds(form) {
-    var formData = getFormObject(form);
-    var hotelsId = Object.keys(hotels);
-    Object.keys(hotelFilters).forEach(function (name) {
-      if (formData[name] !== 'any' &&
-          formData[name][0] !== undefined) {
-        hotelsId = hotelFilters[name](hotelsId, formData[name]);
-      }
-    });
-    return hotelsId;
-  }
-
-  function filterByType(hotelsId, value) {
-    return hotelsId.filter(function (id) {
-      return hotels[id].offer.type === value;
-    });
-  }
-
-  function filterByPrice(hotelsId, value) {
-    var min = priceRangeDescription[value].min;
-    var max = priceRangeDescription[value].max;
-
-    return hotelsId.filter(function (id) {
-      return hotels[id].offer.price >= min && hotels[id].offer.price < max;
-    });
-  }
-
-  function filterByRooms(hotelsId, value) {
-    return hotelsId.filter(function (id) {
-      return hotels[id].offer.rooms === +value;
-    });
-  }
-
-  function filterByGuests(hotelsId, value) {
-    return hotelsId.filter(function (id) {
-      return hotels[id].offer.guests === +value;
-    });
-  }
-
-  function filterByFeatures(hotelsId, features) {
-    if (features[0] === undefined) {
-      return hotelsId;
-    }
-    return hotelsId.filter(function (id) {
-      var matched = true;
-      for (var i = 0; i < features.length; i++) {
-        if (!hotels[id].offer.features.includes(features[i])) {
-          matched = false;
-          break;
-        }
-      }
-      return matched;
-    });
-  }
-
-  function getFormObject(form) {
-    var formData = new FormData(form);
-    var formObj = {};
-    Object.keys(hotelFilters).forEach(function (name) {
-      formObj[name] = name !== 'features' ? formData.get(name) : formData.getAll(name);
-    });
-    return formObj;
-  }
-
-  // ####################################
-  // ######       END MODULE7      ######
-  // ####################################
 
   disableKeksobooking();
 
   window.main = {
     disableKeksobooking: disableKeksobooking,
-    pinClickHandler: pinClickHandler,
-    pinKeyEnterHandler: pinKeyEnterHandler,
+    renderPinById: renderPinById,
   };
 
 })();
