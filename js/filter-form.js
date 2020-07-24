54856f5ed43fb9ba3de7cabbeeab2f0124414b26
ildarjan <nobap84@rambler.ru>
2020-07-25 00:00:58 +0400

    make iteration through filters ugly but shortly. export one enable fn

--- js/filter-form.js
+++ js/filter-form.js
@@ -1,18 +1,18 @@
 'use strict';
 
 (function () {
+  var DEBOUNCE_DELAY = 500;
 
-  var activatePins = window.hotelsPins.activatePins;
   var removeCurrentCard = window.hotelsCards.removeCurrentCard;
   var removeRenderedPins = window.hotelsPins.removeRenderedPins;
   var renderHotelsPins = window.hotelsPins.renderPins;
+  var mapFilters = window.keksobookingMap.mapFilters;
 
   var hotelsInputFilters = {
-    'housing-type': filterByType,
-    'housing-price': filterByPrice,
-    'housing-guests': filterByGuests,
-    'housing-rooms': filterByRooms,
-
+    'housing-type': matchedHotelByType,
+    'housing-price': matchedHotelByPrice,
+    'housing-guests': matchedHotelByGuests,
+    'housing-rooms': matchedHotelByRooms,
   };
 
   var hotelsFeaturesFilter = {
@@ -34,85 +34,106 @@
     },
   };
 
-  var mapFilters = window.keksobookingMap.mapFilters;
+  var hotels;
+  var hotelsIds = [];
+  var maxPinsDisplay;
 
-  function applyFormFilters(hotels, maxPins) {
-    var filteredIds = getFilteredHotelsIds(mapFilters, hotels).slice(0, maxPins);
+  function enableFilterForm(hotelsObj, maxPins) {
+    hotels = hotelsObj;
+    maxPinsDisplay = maxPins;
+    var debouncedFilters = window.utilities.debounce(applyFormFilters, DEBOUNCE_DELAY);
+    mapFilters.addEventListener('change', function () {
+      debouncedFilters();
+    });
+  }
+
+  function applyFormFilters() {
+    var appliedFilters = getSelectedFilters(mapFilters);
+    hotelsIds = filterHotelsIds(appliedFilters, maxPinsDisplay);
     removeRenderedPins();
     removeCurrentCard();
-    renderHotelsPins(filteredIds, hotels);
-    activatePins();
+    renderHotelsPins(hotelsIds, hotels);
   }
 
-  function getFilteredHotelsIds(form, hotels) {
-    var formData = getFilterFormObject(form);
-    var hotelsId = Object.keys(hotels);
-    Object.keys(hotelsInputFilters).forEach(function (name) {
-      if (formData[name] !== 'any' &&
-          formData[name][0] !== undefined) {
-        hotelsId = hotelsInputFilters[name](hotels, hotelsId, formData[name]);
-      }
-    });
-    return hotelsId;
-  }
-
-  function filterByType(hotels, hotelsId, value) {
-    return hotelsId.filter(function (id) {
-      return hotels[id].offer.type === value;
-    });
-  }
-
-  function filterByPrice(hotels, hotelsId, value) {
-    var min = priceRangeDescription[value].min;
-    var max = priceRangeDescription[value].max;
-
-    return hotelsId.filter(function (id) {
-      return hotels[id].offer.price >= min && hotels[id].offer.price < max;
-    });
-  }
-
-  function filterByRooms(hotels, hotelsId, value) {
-    return hotelsId.filter(function (id) {
-      return hotels[id].offer.rooms === +value;
-    });
-  }
-
-  function filterByGuests(hotels, hotelsId, value) {
-    return hotelsId.filter(function (id) {
-      return hotels[id].offer.guests === +value;
-    });
-  }
-
-  function filterByFeatures(hotels, hotelsId, features) {
-    if (features[0] === undefined) {
-      return hotelsId;
-    }
-    return hotelsId.filter(function (id) {
-      var matched = true;
-      for (var i = 0; i < features.length; i++) {
-        if (!hotels[id].offer.features.includes(features[i])) {
-          matched = false;
-          break;
-        }
-      }
-      return matched;
-    });
-  }
-
-  function getFilterFormObject(form) {
+  function getSelectedFilters(form) {
     var formData = new FormData(form);
-    var result = {};
+    var result = [];
     Object.keys(hotelsInputFilters).forEach(function (name) {
-      result[name] = formData.get(name);
+      var selectedValue = formData.get(name);
+      if (selectedValue !== 'any') {
+        result.push({
+          filterName: hotelsInputFilters[name],
+          value: selectedValue
+        });
+      }
     });
     Object.keys(hotelsFeaturesFilter).forEach(function (name) {
-      result[name] = formData.getAll(name);
+      var selectedFeatures = formData.getAll(name);
+      if (selectedFeatures.length > 0) {
+        result.push({
+          filterName: hotelsFeaturesFilter[name],
+          value: selectedFeatures
+        });
+      }
     });
     return result;
   }
 
+  function filterHotelsIds(selectedFilters, maxPins) {
+    var resultIds = [];
+    hotelsIds = Object.keys(hotels);
+    for (var i = 0; i < hotelsIds.length; i++) {
+      if (resultIds.length >= maxPins) {
+        break;
+      }
+      var id = hotelsIds[i];
+      var matched = true;
+      for (var j = 0; j < selectedFilters.length; j++) {
+        var matchSelectedFilter = selectedFilters[j].filterName;
+        var value = selectedFilters[j].value;
+        if (!matchSelectedFilter(hotels[id], value)) {
+          matched = false;
+          break;
+        }
+      }
+      if (matched) {
+        resultIds.push(hotelsIds[i]);
+      }
+    }
+    return resultIds;
+  }
+
+  function matchedHotelByType(hotel, value) {
+    return hotel.offer.type === value;
+  }
+
+  function matchedHotelByPrice(hotel, value) {
+    var min = priceRangeDescription[value].min;
+    var max = priceRangeDescription[value].max;
+    return hotel.offer.price >= min && hotel.offer.price < max;
+  }
+
+  function matchedHotelByRooms(hotel, value) {
+    return hotel.offer.rooms === +value;
+  }
+
+  function matchedHotelByGuests(hotel, value) {
+    return hotel.offer.guests === +value;
+  }
+
+  function filterByFeatures(hotel, features) {
+    var featuresMatched = true;
+    for (var i = 0; i < features.length; i++) {
+      if (!hotel.offer.features.includes(features[i])) {
+        featuresMatched = false;
+        break;
+      }
+    }
+    return featuresMatched;
+  }
+
   window.filterForm = {
-    applyPinsFilters: applyFormFilters
+    enableFilterForm: enableFilterForm
   };
 
 })();
